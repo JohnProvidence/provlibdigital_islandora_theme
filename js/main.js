e513f849999125bfb9f2dd07924dfe9a1acfdd09
Digital Content Coordinator <dcc@Digitals-iMac.local>
2018-12-11 15:26:32 -0500

    added sticky sidenav

--- js/main.js
+++ js/main.js
@@ -86,7 +86,23 @@ jQuery(document).ready(function($) {
 		br.init();
 	}
 	
+	// sticky sidebar
+	var $sidebar = $('.pld-facets-block'),
+		$window = $(window),
+		offset = $sidebar.offset();
+		topPadding = 50;
 
+		$window.scroll(function() {
+			if($window.scrollTop() > offset.top) {
+				$sidebar.stop().animate({
+					marginTop: $window.scrollTop() - offset.top + topPadding
+				});
+			} else {
+				$sidebar.stop().animate({
+					marginTop: 0
+				});
+			}
+		});
 	
 
 });
