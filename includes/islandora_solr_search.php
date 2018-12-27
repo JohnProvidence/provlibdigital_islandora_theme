e6a9c17c4ada66b14aa0891dcf53ec02213ad3fa
Digital Content Coordinator <dcc@Digitals-iMac.local>
2018-12-27 10:09:02 -0500

    derp part deux

--- includes/islandora_solr_search.php
+++ includes/islandora_solr_search.php
@@ -64,10 +64,10 @@ function pld_preprocess_islandora_solr_grid(&$variables) {
 			
 			if($object != NULL):
 				$obj_models = $object->relationships->get('info:fedora/fedora-system:def/model#', 'hasModel');
-				var_dump($obj_models);
 				if($obj_models != FALSE):
 				
 					$obj_model = $obj_models[0]['object']['value'];
+					var_dump($obj_model);
 					$copyright = $object->getDatastream('COPYRIGHT');
 
 					if($obj_model == 'islandora:compoundCModel'):
